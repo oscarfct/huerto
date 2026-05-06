3a9ac7fec5b088f9a348cf902f33397e259c83cc
oscarfct <105998508+oscarfct@users.noreply.github.com>
2026-05-06 08:05:27 -0500

    Actualizar main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-ENCENDER_WS = ""
+def mostrar():
+    if ENCENDER_WS == 1:
+        basic.show_icon(IconNames.UMBRELLA)
+    else:
+        basic.show_icon(IconNames.ASLEEP)
+        basic.pause(5000)
+ENCENDER_WS = 0
+ENCENDER_WS = 1
 BOMBAAGUA_PIN = DigitalPin.P0
-ENCENDIDO_WS = 1 * 30 * 1000
+ENCENDIDO_WS = 1 * 20 * 1000
 APAGADO_WS = 10 * 60 * 60 * 1000
-TIEMPO_WS = "" + str(control.event_timestamp())
-
-def on_button_pressed_b():
-    basic.show_string("T:" + ("" + str(input.temperature())) + "ºC")
-input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_forever():
-    mostrar()    
+    mostrar()
 forever(on_forever)
 
-def onIn_background():
+def on_in_background():
+    global ENCENDER_WS
     while True:
-            pins.digital_write_pin(BOMBAAGUA_PIN, 1)
-            ENCENDER_WS = "on"
-            basic.pause(ENCENDIDO_WS)
-            pins.digital_write_pin(BOMBAAGUA_PIN, 0)
-            ENCENDER_WS = "off"
-            basic.pause(APAGADO_WS)
-control.in_background(onIn_background)
-
-def mostrar():
-    if ENCENDER_WS == "on":
-        basic.show_icon(IconNames.YES)
-    if ENCENDER_WS == "off" :
-        basic.show_icon(IconNames.NO)
+        pins.digital_write_pin(BOMBAAGUA_PIN, 1)
+        ENCENDER_WS = 1
+        basic.pause(ENCENDIDO_WS)
+        pins.digital_write_pin(BOMBAAGUA_PIN, 0)
+        ENCENDER_WS = 0
+        basic.pause(APAGADO_WS)
+control.in_background(on_in_background)
--- main.ts
+++ main.ts
@@ -1,29 +1,27 @@
 function mostrar () {
-    if (ENCENDER_WS == "1") {
-        basic.showIcon(IconNames.Umbrella)
-    } else {
-        basic.showIcon(IconNames.Asleep)
+    if (ENCENDER_WS == 1) {
+    	
     }
 }
-input.onButtonPressed(Button.B, function () {
-    basic.showString("T:" + ("" + input.temperature()) + "ºC")
-})
-let ENCENDER_WS = ""
+let ENCENDER_WS = 0
+ENCENDER_WS = 1
 let BOMBAAGUA_PIN = DigitalPin.P0
-let ENCENDIDO_WS = 1 * 30 * 1000
+let ENCENDIDO_WS = 1 * 20 * 1000
 let APAGADO_WS = 10 * 60 * 60 * 1000
-let TIEMPO_WS = "" + control.eventTimestamp()
 forever(function on_forever() {
     mostrar()
 })
 control.inBackground(function () {
-    let ENCENDER_WS2: string;
-while (true) {
+    while (true) {
         pins.digitalWritePin(BOMBAAGUA_PIN, 1)
-        ENCENDER_WS += 1
+        ENCENDER_WS = 1
+        basic.showIcon(IconNames.Umbrella)
         basic.pause(ENCENDIDO_WS)
         pins.digitalWritePin(BOMBAAGUA_PIN, 0)
-        ENCENDER_WS += 0
+        ENCENDER_WS = 0
+        basic.showIcon(IconNames.Asleep)
+        basic.pause(5000)
+        basic.clearScreen()
         basic.pause(APAGADO_WS)
     }
 })
