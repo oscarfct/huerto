24524b55b64fcb929fc344cdf1fb4bf8c6ffd073
oscarfct <105998508+oscarfct@users.noreply.github.com>
2026-05-06 08:26:19 -0500

    Actualizar pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-def mostrar():
-    if ENCENDER_WS == 1:
-        basic.show_icon(IconNames.UMBRELLA)
-    else:
-        basic.show_icon(IconNames.ASLEEP)
-        basic.pause(5000)
-ENCENDER_WS = 0
 ENCENDER_WS = 1
 BOMBAAGUA_PIN = DigitalPin.P0
 ENCENDIDO_WS = 1 * 20 * 1000
 APAGADO_WS = 10 * 60 * 60 * 1000
 
-def on_forever():
-    mostrar()
-forever(on_forever)
 
 def on_in_background():
     global ENCENDER_WS
     while True:
         pins.digital_write_pin(BOMBAAGUA_PIN, 1)
         ENCENDER_WS = 1
+        basic.show_icon(IconNames.UMBRELLA)
         basic.pause(ENCENDIDO_WS)
         pins.digital_write_pin(BOMBAAGUA_PIN, 0)
         ENCENDER_WS = 0
+        basic.show_icon(IconNames.ASLEEP)
+        basic.pause(5000)
+        basic.clear_screen()
         basic.pause(APAGADO_WS)
 control.in_background(on_in_background)
--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
-function mostrar () {
-    if (ENCENDER_WS == 1) {
-    	
-    }
-}
-let ENCENDER_WS = 0
-ENCENDER_WS = 1
+let ENCENDER_WS = 1
 let BOMBAAGUA_PIN = DigitalPin.P0
-let ENCENDIDO_WS = 1 * 20 * 1000
-let APAGADO_WS = 10 * 60 * 60 * 1000
-forever(function on_forever() {
-    mostrar()
-})
+let ENCENDIDO_WS = 1 * 60 * 1000
+let APAGADO_WS = 6 * 60 * 60 * 1000
+datalogger.setColumnTitles(
+"temperatura",
+"luz"
+)
+datalogger.includeTimestamp(FlashLogTimeStampFormat.Hours)
 control.inBackground(function () {
     while (true) {
         pins.digitalWritePin(BOMBAAGUA_PIN, 1)
         ENCENDER_WS = 1
+        datalogger.log(
+        datalogger.createCV("luz", input.lightLevel()),
+        datalogger.createCV("temperatura", input.temperature())
+        )
         basic.showIcon(IconNames.Umbrella)
         basic.pause(ENCENDIDO_WS)
         pins.digitalWritePin(BOMBAAGUA_PIN, 0)
