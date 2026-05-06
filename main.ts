e985bcd1579ae615389ad56b38aba09f34c5aa09
oscarfct <105998508+oscarfct@users.noreply.github.com>
2026-05-06 08:34:53 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,25 @@
+function LEDoff () {
+    basic.showIcon(IconNames.Asleep)
+    basic.pause(5000)
+    basic.clearScreen()
+}
+function LEDon () {
+    basic.showIcon(IconNames.Umbrella)
+}
 let ENCENDER_WS = 1
 let BOMBAAGUA_PIN = DigitalPin.P0
 let ENCENDIDO_WS = 1 * 60 * 1000
 let APAGADO_WS = 6 * 60 * 60 * 1000
-datalogger.setColumnTitles(
-"temperatura",
-"luz"
-)
-datalogger.includeTimestamp(FlashLogTimeStampFormat.Hours)
+radio.setGroup(1)
 control.inBackground(function () {
     while (true) {
         pins.digitalWritePin(BOMBAAGUA_PIN, 1)
+        LEDon()
         ENCENDER_WS = 1
-        datalogger.log(
-        datalogger.createCV("luz", input.lightLevel()),
-        datalogger.createCV("temperatura", input.temperature())
-        )
-        basic.showIcon(IconNames.Umbrella)
         basic.pause(ENCENDIDO_WS)
         pins.digitalWritePin(BOMBAAGUA_PIN, 0)
         ENCENDER_WS = 0
-        basic.showIcon(IconNames.Asleep)
-        basic.pause(5000)
-        basic.clearScreen()
+        LEDoff()
         basic.pause(APAGADO_WS)
     }
 })
